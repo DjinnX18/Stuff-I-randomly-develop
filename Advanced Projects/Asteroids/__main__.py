3d1aff6b7eded69f3c9d782b25203e106e94e817
Swindell17520 <59296149+Swindell17520@users.noreply.github.com>
2022-06-19 14:37:13 +0200

    Add files via upload

---
+++ Advanced Projects/Asteroids/__main__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  3 22:38:15 2021
+
+@author: Rahul
+"""
+import Asteroid
+if __name__ == "__main__":
+    a = Asteroid.Asteroids()
+    a.main_loop()
+    
